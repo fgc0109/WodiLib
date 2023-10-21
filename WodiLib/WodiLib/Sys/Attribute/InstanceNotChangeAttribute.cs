a151655cb9699c449bc54c801cf5f480ca2291e8
カメスケ <winterer_snowguard_2@yahoo.co.jp>
2023-10-21 19:35:23 +0900

    InstanceNotChangedAttribute のドキュメントコメントを修正

--- WodiLib/WodiLib/Sys/Attribute/InstanceNotChangeAttribute.cs
+++ WodiLib/WodiLib/Sys/Attribute/InstanceNotChangeAttribute.cs
@@ -12,12 +12,11 @@ using System.ComponentModel;
 namespace WodiLib.Sys
 {
     /// <summary>
-    ///     変更されないプロパティであることを示す属性。
+    ///     インスタンスが変更されないプロパティであることを示す属性。
     /// </summary>
     /// <remarks>
-    ///     この属性が付与されたプロパティはインスタンスが変化しない。
-    ///     そのため <see cref="INotifyPropertyChanging.PropertyChanging"/> および
-    ///     <see cref="INotifyPropertyChanged.PropertyChanged"/> も通知されることはない。
+    ///     この属性が付与されたプロパティはSetterが設けられず、
+    ///     参照するインスタンスがクラスメソッドの処理によって変更されることもない。
     /// </remarks>
     [EditorBrowsable(EditorBrowsableState.Never)]
     [AttributeUsage(AttributeTargets.Property)]
